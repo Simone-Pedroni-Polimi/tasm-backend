c7df40823d2c9421848493568d372cbd7b93e73c
AlessiaBerni <102222336+AlessiaBerni@users.noreply.github.com>
2025-04-24 11:45:51 +0200

    Update getData.js

--- api/getData.js
+++ api/getData.js
@@ -8,7 +8,7 @@ const supabase = createClient(
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     const { data, error } = await supabase
-      .from('nome_tabella')  // Sostituisci con il nome della tua tabella
+      .from('todos')  // Sostituisci con il nome della tua tabella
       .select('*');
       
     if (error) {
