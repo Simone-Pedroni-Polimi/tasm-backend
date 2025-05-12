8b95bf10b2bf94108af14553fa2101d68c68125b
AlessiaBerni <102222336+AlessiaBerni@users.noreply.github.com>
2025-05-12 10:05:56 +0200

    Update getAllTeachers.js

--- api/getAllTeachers.js
+++ api/getAllTeachers.js
@@ -1,15 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,  // La URL di Supabase
-  process.env.SUPABASE_KEY   // La chiave API di Supabase
+  process.env.SUPABASE_URL, 
+  process.env.SUPABASE_KEY
 );
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    
-    try {
-      // Recupera il todo con il dato id dalla tabella "todos"
+    // Recupera il todo con il dato id dalla tabella "todos"
       /*Recupera info base dei teacher: 
           IdTeacher, 
           Name, 
@@ -22,25 +20,13 @@ export default async function handler(req, res) {
         .select('TeacherId, Name,Mantra,MainImageURL ')
         .single(); // Usa .single() per ottenere un singolo risultato (se esiste)
 
-      if (error) {
-        // Se c'è un errore con Supabase, restituisci un errore 500
-        return res.status(500).json({ error: error.message });
-      }
-
-      if (!data) {
-        // Se il dato non esiste per l'ID specificato, ritorna 404
-        return res.status(404).json({ error: 'ToDo non trovato' });
-      }
-
-      // Rispondi con i dati ottenuti
-      return res.status(200).json(data);
-    } catch (err) {
-      // Gestisce eventuali errori nel codice
-      console.error('Errore interno', err);
-      return res.status(500).json({ error: 'Errore interno del server' });
+      
+    if (error) {
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(200).json(data);
     }
   } else {
-    // Se il metodo non è POST, ritorna 405 (Method Not Allowed)
     res.status(405).json({ error: 'Metodo non permesso' });
   }
 }
