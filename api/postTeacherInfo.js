f078cf9c8212047f5406721739647af6d4b6fb71
AlessiaBerni <102222336+AlessiaBerni@users.noreply.github.com>
2025-05-13 10:18:17 +0200

    Update postTeacherInfo.js

--- api/postTeacherInfo.js
+++ api/postTeacherInfo.js
@@ -20,8 +20,8 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { TeacherId } = req.body;
 console.log('Richiesta ricevuta con TeacherId:', req.body);
-    if (TeacherId) {
-      return res.status(400).json({ error: 'ID mancante', TeacherId });
+    if (!TeacherId) {
+      return res.status(400).json({ error: 'ID mancante' });
     }
 
     try {
