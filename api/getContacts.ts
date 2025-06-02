fd7f871f6226c4053d3fc17f20ac21763438a262
Simone-Pedroni-Polimi <87439991+Simone-Pedroni-Polimi@users.noreply.github.com>
2025-06-02 14:04:32 +0200

    getContacts.ts file

---
+++ api/getContacts.ts
@@ -0,0 +1,30 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node"
+import { Contact } from "../lib/types/responses.types"
+import { supabase } from "../lib/supabase"
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end()
+  }
+
+  const { data, error } = await supabase.from("Contact").select(`
+      ContactId,
+      ContactInfo
+    `)
+
+  if (error) {
+    res.status(500).json({ error: error.message })
+  } else {
+    const contacts: Contact[] = data.map(() => ({
+        contactId: data?.ContactId ?? "No ID",
+        contactInfo: data?.ContactInfo ?? "No info",
+      })
+    )
+
+    res.status(200).json(contacts)
+  }
+}
